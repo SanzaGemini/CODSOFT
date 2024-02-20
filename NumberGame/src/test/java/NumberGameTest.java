edb9afca4923584d8042cbe5916712e7a5284bb8
SanzaGemini <136991241+SanzaGemini@users.noreply.github.com>
2024-02-20 11:25:28 +0200

    Update NumberGameTest.java
    Get Answer Tests Added

--- NumberGame/src/test/java/NumberGameTest.java
+++ NumberGame/src/test/java/NumberGameTest.java
@@ -8,4 +8,15 @@ public class NumberGameTest {
         NumberGame numberGame = new NumberGame();
         Assertions.assertTrue(numberGame.generateNumber(9)<10);
     }
+    
+    @Test
+    public void getAnswer(){
+
+        String guess = "Guess\n10\n";
+        InputStream inputStream = new ByteArrayInputStream(guess.getBytes());
+
+        numberGame = new NumberGame(inputStream);
+
+        Assertions.assertEquals("10",numberGame.getAnswer());
+    }
 }
