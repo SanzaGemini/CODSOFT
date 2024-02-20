f2046b6d4493d9c5be4eee24a29300487c66954f
SanzaGemini <136991241+SanzaGemini@users.noreply.github.com>
2024-02-20 11:30:05 +0200

    Update NumberGame.java
    get Answer And validate Input methods added

--- NumberGame/src/main/java/NumberGame.java
+++ NumberGame/src/main/java/NumberGame.java
@@ -15,4 +15,20 @@ public class NumberGame {
     public int generateNumber(int maxNumber) {
         return random.nextInt(maxNumber);
     }
+
+    public String getAnswer() {
+        String answer;
+        try {
+            System.out.println("Guess The Number?");
+            answer = scanner.nextLine();
+            return validateInput(answer) ? answer : getAnswer();
+        } catch (NoSuchElementException e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public boolean validateInput(String userInput) {
+        return userInput.matches("\\d+");
+    }
 }
